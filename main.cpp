69e40abb30cd43f7fc9c15a26cfd7780c2aebb77
berkaykarlik <brkykrlk@gmail.com>
2023-07-22 23:31:41 +0300

    ADD: pwd and cd

--- main.cpp
+++ main.cpp
@@ -1,24 +1,44 @@
 #include <iostream>
 #include <string>
 #include "util.cpp"
+#include "unistd.h"
 
-const char* PROMPT = "pawsh>>";
-
+const char* PROMPT = "pawsh|";
+const char* GREEN =  "\033[1;32m";
+const char* END = "\033[0m";
 
 int main(){
     std::string cmd;
+    std::vector<std::string> split_cmd;
+    
+    getPwd();
 
     //event loop
     while(true){
-        std::cout << PROMPT;
+        std::cout << GREEN << PROMPT << END;
+        if(pwd)
+            std::cout << "" << pwd;
+        std::cout  << GREEN << "|>>"  << END;
+
         std::getline(std::cin, cmd);
         cmd = stripWhitespace(cmd);
+        split_cmd = splitStringByWS(cmd);
+        cmd = split_cmd[0];
         // diffirent from invalid command case so we explicitly continue
-        if(cmd == ""){ 
+        if(cmd == ""){
             continue;
         }
-        else if(cmd == "exit"){
+        else if(cmd == "exit")
             return 0;
+        else if(cmd == "pwd"){
+            if (getPwd() ==0)
+                std::cout << pwd << std::endl;
+            else
+                std::cerr << "Couldn't get pwd" << std::endl;
+        }
+        else if(cmd == "cd"){
+            chdir(split_cmd[1].c_str());
+            getPwd();
         }
         else{
             //check for existing commands, if no such command print invalid
@@ -26,8 +46,5 @@ int main(){
                 std::cout << "Invalid command \"" << cmd << "\"" <<std::endl;
         }
     }
-
-
-
     return 0;
 }
--- util.cpp
+++ util.cpp
@@ -1,7 +1,13 @@
 #include <string>
+#include <vector>
+#include <sstream>
 #include <iostream>
 #include <cstdio>
 #include <array>
+#include <unistd.h>
+#include <climits>
+
+char pwd[PATH_MAX];
 
 std::string stripWhitespace(const std::string& input) {
     // Find the first non-whitespace character
@@ -16,6 +22,17 @@ std::string stripWhitespace(const std::string& input) {
 
     // Extract the substring containing the non-whitespace characters
     return input.substr(firstNonWhitespace, lastNonWhitespace - firstNonWhitespace + 1);
+}
+
+std::vector<std::string> splitStringByWS(const std::string& input){
+    std::vector<std::string> words;
+    std::istringstream iss(input);
+    std::string word;
+
+    while(iss >> word){
+        words.push_back(word);
+    }
+    return words;
 }
 
 int runCommand(const char* cmd) {
@@ -35,3 +52,10 @@ int runCommand(const char* cmd) {
     }
     return pclose(pipe);
 }
+
+int getPwd(){
+    if (getcwd(pwd, sizeof(pwd)) != nullptr)
+        return 0;
+    return 1;
+}
+
